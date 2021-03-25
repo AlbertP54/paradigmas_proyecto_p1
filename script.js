7849caeb180a89655684e0c2c5125ebdb2f360d2
Alberto Payan <a311047@uach.mx>
2021-03-24 18:38:38 -0700

    Creando estilos de la tabla

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-mesaBillar = () => {
+const mesaBillar = () => {
+    let mesa = document.getElementById("mesa");
 
+    if(mesa.src.match('billar apagado')) {
+        mesa.src = "images/billar encendido.png";
+    } else {
+        mesa.src = "images/billar apagado.png";
+    }
 }
