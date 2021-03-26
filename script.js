8cbe7ebe9e2a2aa758683af90cd92e5af666cc84
Alberto Payan <a311047@uach.mx>
2021-03-26 01:14:02 -0700

    Mejorando funcion

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-const mesaBillar = () => {
-    let mesa = document.getElementById("mesa");
+const mesaBillar = (id, hrini, hrfn, total ) => {
+    if(id.src.match('billarOff')){
+        hrfn.innerHTML = "";
+        total.innerHTML = "";
+        
+        const msegPorMin = 1000 * 60;
+        const fecha = new Date();
+        const fechaMseg = fecha.getTime();
 
-    if(mesa.src.match('billar apagado')) {
-        mesa.src = "images/billar encendido.png";
+        const intervalo = fechaMseg - fecha.getTime();
+        
+        const mins = Math.floor(intervalo / msegPorMin);
+        intervalo = intervalo - (mins * msegPorMin);
+
+        const seg = Math.floor(intervalo / 1000);
+
+        hrini.innerHTML = mins + ":" + seg;
+        id.src = "images/billarOn.png"
     } else {
-        mesa.src = "images/billar apagado.png";
-    }
+        const msegPorMin = 1000 * 60;
+        const fecha = new Date();
+        const fechaMseg = fecha.getTime();
+
+        const intervalo = fechaMseg - fecha.getTime();
+        
+        const mins = Math.floor(intervalo / msegPorMin);
+        intervalo = intervalo - (mins * msegPorMin);
+
+        const seg = Math.floor(intervalo / 1000);
+        const precio = (parseInt(mins) * 50) + (parseInt(seg) * 0.8333); 
+
+        hrfn.innerHTML = min + ":" + seg;
+        total.innerHTML = "$" + precio;
+        id.src = "images/billarOff.png";
+    }   
 }
